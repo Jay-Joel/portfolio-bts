4882d370e656656e29a3fd2f5366208a5d782684
Jay Joël MASSAMBA <massambajayjoel@hotmail.com>
2026-03-13 12:16:21 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/portfolio-bts/"  // garde cette ligne pour GitHub Pages
 });
